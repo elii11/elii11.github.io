23acf1e8a60587bdac9b1d175f7d1d2d21f4fd50
Elizear John Acosta Galapia <32735402+elii11@users.noreply.github.com>
2018-01-24 16:45:26 +0800

    Personal Website

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function() {
+  var widthW = $('.wrapper').width();
+  if(widthW < 800) {
+    $('.ham').on('click', function(e) {
+      e.preventDefault();
+      var navi = $('.navi');
+      var full = $('.full-nav');
+
+      if(navi.hasClass('nav-grid')) {
+
+        navi.toggleClass('nav-grid');
+        full.animate({top: '-100%'},400);
+        full.animate({left: '-100%'},400);
+      } else {
+        navi.toggleClass('nav-grid');
+        full.animate({left: '0%'},100);
+        full.animate({top: '5%'},700);
+      }
+      $('.navi').addClass('shadow');
+      $('.ham > div:nth-child(1)').toggleClass('rotate');
+      $('.ham > div:nth-child(2)').toggleClass('hidden-ham');
+      $('.ham > div:nth-child(3)').toggleClass('rotate3');
+
+    });
+
+  } else {
+
+    $('.ham').on('click', function(e) {
+      e.preventDefault();
+      var navi = $('.navi');
+      var full = $('.full-nav');
+
+      if(navi.hasClass('slideInRight')) {
+
+        navi.removeClass('animated slideInRight');
+        navi.addClass('animated slideInLeft').delay(400);
+        navi.toggleClass('nav-grid');
+        full.animate({left: '-100%'},400);
+      } else {
+        navi.addClass('animated slideInRight');
+        navi.toggleClass('nav-grid');
+        full.animate({left: '0%'},700);
+      }
+      $('.navi').addClass('shadow');
+      $('.ham > div:nth-child(1)').toggleClass('rotate');
+      $('.ham > div:nth-child(2)').toggleClass('hidden-ham');
+      $('.ham > div:nth-child(3)').toggleClass('rotate3');
+
+    });
+  }
+});
